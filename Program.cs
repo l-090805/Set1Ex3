ea157ad7a0f32daf59e138ee19902f737b6be87c
falau <falau@LENOVO>
2024-10-28 11:11:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Set1Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introdu valoarea lui n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Introdu valoarea lui k: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            if( k != 0)
+            {
+                if (n%k == 0)
+                {
+                    Console.WriteLine($"Numarul {n} este dibizibil cu {k}");
+                }
+                else
+                {
+                    Console.WriteLine($"Numarul {n} nu este divizibil cu {k}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Impartirea la 0 nu este posibila");
+            }
+        }
+    }
+}
